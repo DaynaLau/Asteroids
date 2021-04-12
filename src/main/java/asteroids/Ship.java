e566b6a9f8978a059acac7746ce10a4fa3976d4f
DaynaLau <73656477+DaynaLau@users.noreply.github.com>
2021-04-12 16:07:16 -0400

    adding src files

---
+++ src/main/java/asteroids/Ship.java
@@ -0,0 +1,19 @@
+
+package asteroids;
+
+
+import javafx.scene.shape.Polygon;
+
+/**
+ *
+ * @author dayna
+ */
+public class Ship extends Character{
+   
+    public Ship(int x, int y) {
+        super(new Polygon(-5, -5, 10, 0, -5, 5), x, y);
+    }
+    
+   
+    
+}
